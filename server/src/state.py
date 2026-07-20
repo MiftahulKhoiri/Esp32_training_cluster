6ae7751748ce88735aaebcc65a74fb5b3c2710a5
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-20 17:19:33 +0800

    Update state.py

--- server/src/state.py
+++ server/src/state.py
@@ -4,13 +4,14 @@ import threading
 
 lock = threading.Lock()
 
-vocab_size = None       # int, diisi = tokenizer.vocab_size() aktual
-param_sizes = None      # dict dari config.compute_param_sizes(vocab_size)
-baseline_loss = None    # float, dari config.compute_baseline_loss(vocab_size)
+vocab_size = None
+param_sizes = None
+baseline_loss = None
 
-global_weights = None   # np.ndarray, diinisialisasi random SETELAH vocab_size diketahui
+global_weights = None
 pending_updates = {}
 round_number = 0
+round_start_time = None   # timestamp mulai kumpul weight ronde ini, None kalau belum ada yang lapor
 training_complete = False
 last_eval_loss = None
 
@@ -19,3 +20,5 @@ eval_context = None
 eval_target = None
 
 tokenizer = None
+
+active_nodes = {}   # node_id -> timestamp heartbeat/lapor terakhir
